9450fe6dffda00d1a67f20d9660b11b02b200bb8
MFeuer23 <mallory.feuer@gmail.com>
2018-08-15 14:57:44 -0400

    added to new show form

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     resources :shows, only: [:show, :index, :new, :create, :edit, :update]
   end
 
+    resources :shows
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     root 'application#hello'
 end
