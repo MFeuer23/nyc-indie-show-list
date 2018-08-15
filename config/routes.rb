c71afd23abea4213a2f43945e70553322a186d2d
MFeuer23 <mallory.feuer@gmail.com>
2018-08-15 12:53:34 -0400

    added artist show route and link to navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :artists, controllers: { omniauth_callbacks: 'artists/omniauth_callbacks' }
-
+  resources :artists, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     root 'application#hello'
 end
