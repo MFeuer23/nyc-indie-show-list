991b87ee5cdd658284afc72000c9e7521cb51e0a
MFeuer23 <mallory.feuer@gmail.com>
2018-08-15 15:15:59 -0400

    added to new show form and show controller

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -12,4 +12,14 @@ class ShowsController < ApplicationController
       @show = Show.new(artist_id: params[:artist_id])
   end
 
+  def create
+    #params: {"utf8"=>"✓", "authenticity_token"=>"1AaXo9FRdu25OcwBenmjqh5s4p0qkLXn9MO1E8ua7urwQahtY9aA7HTgnVu3YAmdFsctGI0J5/uQwEmiaYcs4A==", "show"=>{"artist_id"=>"6", "date"=>"12/26/2018", "info"=>"best show ever", "venue_id"=>"1"}, "venue"=>{"name"=>"pianos", "address"=>"ludlow st", "contact"=>"pianosnyc@pianos.com"}, "commit"=>"Create Show", "controller"=>"shows", "action"=>"create"} permitted: false>
+  end
+
+
+  private
+
+  def song_params
+    params.require(:song).permit(:artist_id, :date, :info, :venue_id, :venue)
+  end
 end
