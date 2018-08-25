fbe08d405fc5f10d1a039b04b62333397c595eda
MFeuer23 <mallory.feuer@gmail.com>
2018-08-25 17:29:24 -0400

    edited show controller, made show show page)

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -13,25 +13,24 @@ class ShowsController < ApplicationController
   end
 
   def create
-    binding.pry
+
     @show = Show.new(show_params)
-    @show.date = Date.parse(params[:show][:date])
+    @show.date = Date.parse("#{params.require(:show).permit(:date)}")
     if @show.venue_id
       @show.save
     else
-      @venue = Venue.create(params[:venue])
+      @venue = Venue.create(venue_params)
       @show.venue_id = @venue_id
       @show.save
-
     end
-    #params: {"utf8"=>"✓", "authenticity_token"=>"1AaXo9FRdu25OcwBenmjqh5s4p0qkLXn9MO1E8ua7urwQahtY9aA7HTgnVu3YAmdFsctGI0J5/uQwEmiaYcs4A==", "show"=>{"artist_id"=>"6", "date"=>"12/26/2018", "info"=>"best show ever", "venue_id"=>"1"}, "venue"=>{"name"=>"pianos", "address"=>"ludlow st", "contact"=>"pianosnyc@pianos.com"}, "commit"=>"Create Show", "controller"=>"shows", "action"=>"create"} permitted: false>
+    redirect_to show_path(@show)
   end
 
 
   private
 
   def show_params
-    params.require(:show).permit(:artist_id, :date, :info, :venue_id)
+    params.require(:show).permit(:artist_id, :info, :date, :venue_id)
   end
 
   def venue_params
