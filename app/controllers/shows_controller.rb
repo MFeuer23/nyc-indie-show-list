39a884c59e418553967445d01fc6201d835a101d
MFeuer23 <mallory.feuer@gmail.com>
2018-08-25 19:17:55 -0400

    added edit show functionality

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -25,10 +25,28 @@ class ShowsController < ApplicationController
     redirect_to show_path(@show)
   end
 
-def show
-  @show = Show.find(params[:id])
-end
+  def show
+    @show = Show.find(params[:id])
+  end
 
+  def edit
+    @show = Show.find(params[:id])
+  end
+
+  def update
+    @show = Show.find(params[:id])
+    @show.update(show_params)
+
+    if @show.venue_id
+      @show.save
+    else
+      @venue = Venue.create(venue_params)
+      @show.venue_id = @venue_id
+      @show.save
+    end
+    redirect_to show_path(@show)
+  end
+  
   private
 
   def show_params
