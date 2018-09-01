9508ff769f656a6a3c2924f509945421e49c2e7c
MFeuer23 <mallory.feuer@gmail.com>
2018-09-01 14:49:51 -0400

    added update venue function

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,5 +1,9 @@
 class VenuesController < ApplicationController
 
+  def index
+    @venues = Venue.all.order(name: :asc)
+  end
+
   def show
     @venue = Venue.find(params[:id])
   end
@@ -8,7 +12,22 @@ class VenuesController < ApplicationController
     @venue = Venue.find(params[:id])
   end
 
+  def update
+    @venue = Venue.find(params[:id])
+    @venue.update(venue_params)
+    redirect_to venue_path(@venue)
+  end
+
   def destroy
+    @venue = Venue.find(params[:id])
+    @venue.destroy
+    redirect_to artist_path(current_artist)
+  end
+
+  private
+
+  def venue_params
+    params.require(:venue).permit(:name, :address, :contact)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   devise_for :artists, controllers: { omniauth_callbacks: 'artists/omniauth_callbacks' }
-  resources :artists, only: [:index, :show, :edit, :update, :delete] do
+  resources :artists, only: [:index, :show, :edit, :update] do
     # nested resource
-    resources :shows, only: [:show, :index, :new, :create, :edit, :update, :delete]
+    resources :shows, only: [:show, :index, :new, :create, :edit, :update]
   end
-    resources :venues, only: [:show, :index, :edit, :update, :delete]
+    resources :venues, only: [:show, :index, :edit, :update, :destroy]
     resources :shows
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     root 'application#hello'
