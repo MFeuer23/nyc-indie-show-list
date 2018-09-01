546f0a79d3b522aafe3156281a7508f1b3899f43
MFeuer23 <mallory.feuer@gmail.com>
2018-09-01 14:23:50 -0400

    made venue show page, links to artist show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   devise_for :artists, controllers: { omniauth_callbacks: 'artists/omniauth_callbacks' }
-  resources :artists, only: [:index, :show, :edit, :update] do
+  resources :artists, only: [:index, :show, :edit, :update, :delete] do
     # nested resource
-    resources :shows, only: [:show, :index, :new, :create, :edit, :update]
+    resources :shows, only: [:show, :index, :new, :create, :edit, :update, :delete]
   end
-
+    resources :venues, only: [:show, :index, :edit, :update, :delete]
     resources :shows
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     root 'application#hello'
