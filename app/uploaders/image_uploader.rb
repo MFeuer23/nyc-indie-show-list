c3a93a77acdaa83e0e1ce91a8337ec2f029cf5f3
MFeuer23 <mallory.feuer@gmail.com>
2018-09-15 12:02:44 -0400

    removed thumb size version of photos

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -36,9 +36,6 @@ class ImageUploader < CarrierWave::Uploader::Base
      process resize_to_fit: [300, 200]
    end
 
-   version :thumb do
-     process resize_to_fit: [75, 50]
-   end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
