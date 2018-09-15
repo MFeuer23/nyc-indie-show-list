e33d93328a2372c18293130f772b24c277b35f63
MFeuer23 <mallory.feuer@gmail.com>
2018-09-15 12:59:30 -0400

    made uniquness validation case sensitive

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -2,6 +2,7 @@ class Venue < ApplicationRecord
   has_many :shows
   has_many :artists, through: :shows
 
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
+
   validates :address, presence: true
 end
