387b0492fa6ff0dc3fc3dc1a8e36b52b5dee7add
MFeuer23 <mallory.feuer@gmail.com>
2018-11-21 22:59:20 -0500

    iterated through show objects in ajax response

--- app/assets/javascripts/shows.js
+++ app/assets/javascripts/shows.js
@@ -1,9 +1,13 @@
 $(function () {
   $(".js-view-shows").on('click', function(e) {
-    e.preventDefault;
+    e.preventDefault();
     let id = $(this).data("id");
-    jQuery.get("/artists/" + 6 + ".json", function(data) {
-      console.log(data);
+    jQuery.get("/artists/" + id + ".json", function(data) {
+      $.each(data["shows"], function(index, value) {
+        $(".js-shows").append(value["date"]);
+
+      })
+
     });
   });
 });
