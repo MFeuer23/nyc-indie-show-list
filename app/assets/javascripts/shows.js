25a1c6d1816ddbfd2333fdcdce6dada7caed620a
MFeuer23 <mallory.feuer@gmail.com>
2018-11-22 17:04:52 -0500

    added logic to differentiate between past and upcoming shows

--- app/assets/javascripts/shows.js
+++ app/assets/javascripts/shows.js
@@ -3,8 +3,16 @@ $(function () {
     e.preventDefault();
     let id = $(this).data("id");
     jQuery.get("/artists/" + id + ".json", function(data) {
+      $(".js-shows-upcoming").html("<h4>" + "Upcoming Shows" + "</h4>")
+      $(".js-shows-past").html("<h4>" + "Past Shows" + "</h4>")
       $.each(data["shows"], function(index, value) {
-        $(".js-shows").append(value["date"]);
+
+        if (new Date(value["date"]) >= new Date()) {
+
+          $(".js-shows-upcoming").append("<li>" + value["date"] + "</li>");
+        } else {
+          $(".js-shows-past").append("<li>" + value["date"] + "</li>");
+        }
 
       })
 
