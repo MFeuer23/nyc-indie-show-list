0468844c3b091d04b45c32ec2b5f25f7b089def8
MFeuer23 <mallory.feuer@gmail.com>
2018-11-23 18:28:13 -0500

    made another get request to shows by id

--- app/assets/javascripts/shows.js
+++ app/assets/javascripts/shows.js
@@ -5,6 +5,7 @@ $(function () {
     jQuery.get("/artists/" + id + ".json", function(data) {
       $(".js-shows-upcoming").html("<h4>" + "Upcoming Shows" + "</h4>")
       $(".js-shows-past").html("<h4>" + "Past Shows" + "</h4>")
+      console.log(data)
       $.each(data["shows"], function(index, value) {
         let date = new Date(value["date"])
         if (date >= new Date()) {
@@ -12,12 +13,14 @@ $(function () {
         } else {
           $(".js-shows-past").append("<li>" + formatDate(date) + "</li>");
         }
-
+        $.get("/shows/" + value["id"] + ".json", function(data) {
+          console.log(data)
+        })
       })
-
     });
   });
 });
+                                                    
 function formatDate(date) {
   var monthNames = [
     "January", "February", "March",
