ffcc4e75fc5ee31a64077b69b042f935f9fba8c1
MFeuer23 <mallory.feuer@gmail.com>
2018-11-23 20:13:06 -0500

    formatted date attr

--- app/assets/javascripts/show_show.js
+++ app/assets/javascripts/show_show.js
@@ -2,10 +2,9 @@ $(function () {
   $(".js-next").on("click", function() {
     let nextId = parseInt($(".js-next").attr("data-id")) + 1;
     $.get("/shows/" + nextId + ".json", function(data) {
-      console.log(data)
       $("#artist_name").text(data["artist"]["name"])
       $("#venue_name").text(data["venue"]["name"])
-      $("#show_date").text(data["date"])
+      $("#show_date").text(formatDate(new Date(data["date"])))
       $("#show_info").text(data["info"])
       $("#venue_address").text(data["venue"]["address"])
       $("#show_specific_links").text("")
