2698e7c39354d04359bbc502f0e876e281d4a5f1
MFeuer23 <mallory.feuer@gmail.com>
2018-11-24 18:12:18 -0500

    made list of dates into links

--- app/assets/javascripts/artist_venues_shows.js
+++ app/assets/javascripts/artist_venues_shows.js
@@ -2,18 +2,18 @@ $(function () {
   $(".js-view-venues").on('click', function(e) {
     e.preventDefault();
     let id = $(this).data("id");
-    jQuery.get("/artists/" + id + "/venues.json", function(data) {
+    $.get("/artists/" + id + "/venues.json", function(data) {
       $(".js-venues").html("<h4>" + "Venues Played" + "</h4>")
       let dataUnique = removeDuplicates(data, "id")
+
       console.log(dataUnique)
       $.each(dataUnique, function(index, value) {
-
-
         $(".js-venues").append("<li>" + "<a href='/venues/" + value["id"] + "'>"
         + value["name"] + "</a>" + " // " + "<a href='#' class='js-show-dates' data-id='" + value["id"]
         + "'>See All Show Dates</a>" + "</li>");
         $(".js-venues").append("<ul><div class='" + value["id"] + "'></div></ul>")
       })
+
         $(".js-show-dates").on('click', function(e) {
           e.preventDefault();
           let venue_id = $(this).data("id");
@@ -22,10 +22,11 @@ $(function () {
             console.log(value["id"], venue_id)
             if (value["id"] == venue_id) {
               console.log(value["shows"])
+              
               $.each(value["shows"], function(index, value) {
                 let date = new Date(value["date"])
                 console.log(value["date"])
-                $("." + venue_id).append("<li>" + formatDate(date) + "</li>")
+                $("." + venue_id).append("<a href='/shows/" + value["id"] + "'><li>" + formatDate(date) + "</li></a>")
               })
             }
           })
@@ -33,8 +34,6 @@ $(function () {
     });
   });
 });
-
-
 
 function removeDuplicates(myArr, prop) {
     return myArr.filter((obj, pos, arr) => {
