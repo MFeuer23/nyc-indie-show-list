d0977a29eb3dea812c54dc5c3ee205e4e5f169e2
MFeuer23 <mallory.feuer@gmail.com>
2018-11-24 17:32:52 -0500

    created a function to remove duplicate venues from ajax response

--- app/assets/javascripts/artist_venues_shows.js
+++ app/assets/javascripts/artist_venues_shows.js
@@ -3,9 +3,11 @@ $(function () {
     e.preventDefault();
     let id = $(this).data("id");
     jQuery.get("/artists/" + id + "/venues.json", function(data) {
-      console.log(data)
       $(".js-venues").html("<h4>" + "Venues Played" + "</h4>")
-      $.each(data, function(index, value) {
+      let dataUnique = removeDuplicates(data, "id")
+      console.log(dataUnique)
+      $.each(dataUnique, function(index, value) {
+
         let date = new Date(value["date"])
         $(".js-venues").append("<li>" + "<a href='/venues/" + value["id"] + "'>"
         + value["name"] + "</a>" + " // " + "<a href='#' class='js-show-dates' data-id='" + value["id"]
@@ -20,3 +22,9 @@ $(function () {
     });
   });
 });
+
+function removeDuplicates(myArr, prop) {
+    return myArr.filter((obj, pos, arr) => {
+        return arr.map(mapObj => mapObj[prop]).indexOf(obj[prop]) === pos;
+    });
+}
