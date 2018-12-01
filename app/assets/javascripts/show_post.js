50e7d91bb991ada0cbf3e3771a997a5c73584dca
MFeuer23 <mallory.feuer@gmail.com>
2018-12-01 18:41:53 -0500

    added new Show class with constructor and method

--- app/assets/javascripts/show_post.js
+++ app/assets/javascripts/show_post.js
@@ -4,11 +4,39 @@ $(document).on('turbolinks:load', function () {
 
       var values = $(this).serialize();
       console.log(values);
-debugger;
+
       $.post('/shows.js', values, function(data) {
         console.log(data)
+        debugger;
+        let jsShow = new Show(data)
+
       });
 
 
     });
   });
+
+  class Show {
+    constructor(data) {
+      this.date = data["date"]
+      this.info = data["info"]
+      this.artist = data["artist"]
+      this.venue = data["venue"]
+    }
+    formatDate() {
+        let monthNames = [
+          "January", "February", "March",
+          "April", "May", "June", "July",
+          "August", "September", "October",
+          "November", "December"
+        ];
+        let date = new Date(this.date)
+        let day = date.getDate();
+        let monthIndex = date.getMonth();
+        let year = date.getFullYear();
+
+        return monthNames[monthIndex] + ' ' + day + ', ' + year;
+    }
+
+
+  }
