b7ca3b651840a0d22bc871c6beec969562749ab1
MFeuer23 <mallory.feuer@gmail.com>
2018-12-02 14:15:54 -0500

    added appendToDom function to my show object, removed formatting function

--- app/assets/javascripts/show_post.js
+++ app/assets/javascripts/show_post.js
@@ -2,22 +2,25 @@ $(document).on('turbolinks:load', function () {
   $("form").on("submit", function(e) {
     e.preventDefault();
 
-      var values = $(this).serialize();
+      let values = $(this).serialize();
+      let jsShow;
       console.log(values);
 
-      $.post('/shows.js', values, function(showData) {
+      let showPosting = $.post('/shows.js', values, function(showData) {
         if (!showData["venue"]) {
           $.post('/venues.js', values, function(venueData) {
             showData["venue"] = new Venue(showData, venueData)
-            let jsShow = new Show(showData)
-            console.log(jsShow)
+            jsShow = new Show(showData)
+            jsShow.appendToDom("js-posted-object")
           })
         } else {
-          let jsShow = new Show(showData)
+          jsShow = new Show(showData)
           console.log(jsShow)
+          jsShow.appendToDom("js-posted-object")
         }
+        console.log(jsShow)
       });
-
+      showPosting.done(console.log(jsShow))
 
     });
   });
@@ -29,19 +32,16 @@ $(document).on('turbolinks:load', function () {
       this.artist = data["artist"]
       this.venue = data["venue"]
     }
-    formatDate() {
-        let monthNames = [
-          "January", "February", "March",
-          "April", "May", "June", "July",
-          "August", "September", "October",
-          "November", "December"
-        ];
-        let date = new Date(this.date)
-        let day = date.getDate();
-        let monthIndex = date.getMonth();
-        let year = date.getFullYear();
-
-        return monthNames[monthIndex] + ' ' + day + ', ' + year;
+    appendToDom(class_name) {
+      $(`.${class_name}`).html(
+        "<h3>Posted Show....</h3>" +
+        "<p> Date: " + `${formatDate(new Date(this.date))}` + "</p>" +
+        "<p> Info: " + `${this.info}` + "</p>" +
+        "<p> Artist: " + `${this.artist.name}` + "</p>" +
+        "<p> Venue: " + `${this.venue.name}` + "</p>" +
+        "<p> Address: " + `${this.venue.address}` + "</p>" +
+        "<p> Contact: " + `${this.venue.contact}` + "</p>"
+      )
     }
   }
 
